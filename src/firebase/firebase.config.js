a29d6e6655fdebddcf30dd6aaae3b9245a23ca2c
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2022-10-25 00:01:38 +0600

    file added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDk16wy0ueev-MBhlIVpqQ4y0qhL9N4OH4",
+    authDomain: "smart-learning-cd3d3.firebaseapp.com",
+    projectId: "smart-learning-cd3d3",
+    storageBucket: "smart-learning-cd3d3.appspot.com",
+    messagingSenderId: "503851264940",
+    appId: "1:503851264940:web:30fe5eccd75e219d152809"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
