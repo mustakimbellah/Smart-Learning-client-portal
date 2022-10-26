889cac64af78547d27281c40e4dc36363d268f1c
S.m Mustakim Bellah <59763884+mustakimbellah@users.noreply.github.com>
2022-10-27 00:12:54 +0600

    popup gmail added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -4,10 +4,17 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import photo from '../../../Photo/photo.jpg';
+import Button from 'react-bootstrap/Button';
+import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import { Image } from 'react-bootstrap';
+import { FaUser } from 'react-icons/fa';
 
 const Header = () => {
+    const { user } = useContext(AuthContext);
     return (
-        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Navbar collapseOnSelect className='mb-4' expand="lg" bg="light" variant="light">
             <Container>
                 <img
                     alt="logo"
@@ -30,8 +37,14 @@ const Header = () => {
                             <NavDropdown.Item href="#action/3.1">Light</NavDropdown.Item>
                         </NavDropdown>
                         <Nav.Link eventKey={2} href="#memes">
-                            Dank memes
+                            {user?.photoURL ?
+                                <Image style={{ height: '40px' }} roundedCircle src={user?.photoURL}></Image>
+                                :
+                                <FaUser></FaUser>
+                            }
                         </Nav.Link>
+                        <Nav.Link href="#memes">{user?.displayName}</Nav.Link>
+                        <Link to='/login'><Button variant="outline-success">Log In</Button></Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../layout/Main";
 import Home from '../../Pages/Home/Home';
 import Category from '../../Pages/Category/Category';
+import Login from "../../Pages/Shared/Login/Login";
 
 
 export const routes = createBrowserRouter([
@@ -16,6 +17,10 @@ export const routes = createBrowserRouter([
             {
                 path: '/category/:id',
                 element: <Category></Category>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
             }
         ]
     }
